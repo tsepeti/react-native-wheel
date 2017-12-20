9aa8bcf6d30f0a003f52dab262e845813c9f8ad9
Ioannis Kokkinidis <sudoplz@gmail.com>
2017-12-21 01:17:31 +0200

    Modernizing component
    - Now using prop-types instead of PropTypes
    - Fixed npmignore
    - deleted unused .js file

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-import React,{
-    Component,
-    PropTypes
-} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import ReactNative,{
-    requireNativeComponent
+    requireNativeComponent,
+    PanResponder,
 } from 'react-native';
 
 
 let NativeWheelView = requireNativeComponent('RCTWheelView',WheelView);
 
 
-export default class WheelView extends React.Component{
-    static propTypes = {
-        onItemChange: PropTypes.func,
-        values: PropTypes.array,
-        isLoop: PropTypes.bool,
-        selectedIndex: PropTypes.number,
-        textSize: PropTypes.number,
-        itemsVisible: PropTypes.number,
-        velocityFling: PropTypes.number,
-    };
+class WheelView extends React.Component{
+    constructor(props) {
+        super(props);
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onMoveShouldSetPanResponder: () => true,
+            onShouldBlockNativeResponder: () => true,
+        });
+        this.onItemChange = this.onItemChange.bind(this);
+    }
 
-    _onItemChange(event) {
+    shouldComponentUpdate(nextProps) {
+        return (
+            nextProps.values !== this.props.values
+            ||
+            nextProps.isLoop !== this.props.isLoop
+            ||
+            nextProps.selectedIndex !== this.props.selectedIndex
+            ||
+            nextProps.itemsVisible !== this.props.itemsVisible
+            ||
+            nextProps.velocityFling !== this.props.velocityFling
+        )
+    }
+
+    onItemChange(event) {
         if(this.props.onItemChange){
             this.props.onItemChange(event.nativeEvent.index);
         }
     };
 
     render(){
-        return <NativeWheelView {...this.props} onChange={this._onItemChange.bind(this)} />;
+        return (
+        <NativeWheelView
+            {...this.panResponder.panHandlers}
+            {...this.props}
+            onChange={this.onItemChange}
+        />);
     }
 };
+WheelView.propTypes = {
+    onItemChange: PropTypes.func,
+    values: PropTypes.array,
+    isLoop: PropTypes.bool,
+    selectedIndex: PropTypes.number,
+    textSize: PropTypes.number,
+    itemsVisible: PropTypes.number,
+    velocityFling: PropTypes.number,
+};
+export default WheelView;
 
